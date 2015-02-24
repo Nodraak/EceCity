9a417782e63d83548889d7356e5ac39014470a18
Adrien Chardon <adrienchardon@mailoo.org>
2015-02-24 21:38:19 +0000

    mouhahaha pointeur sur fonction \o/

--- src/ec_allegro_graphic.c
+++ src/ec_allegro_graphic.c
@@ -4,24 +4,24 @@
 #include <ec_allegro.h>
 #include <ec_allegro_graphic.h>
 
-void ec_allegro_graphic_line(BITMAP *s, int x1, int y1, int x2, int y2, int c)
+void _scale_and_call(void(*f)(BITMAP*, int, int, int, int, int), BITMAP* s, int x1, int y1, int x2, int y2, int c)
 {
     x1 = ec_allegro_graphic_scale_x_coord_to_pxl(x1);
     y1 = ec_allegro_graphic_scale_y_coord_to_pxl(y1);
     x2 = ec_allegro_graphic_scale_x_coord_to_pxl(x2);
     y2 = ec_allegro_graphic_scale_y_coord_to_pxl(y2);
 
-    line(s, x1, y1, x2, y2, c);
+    f(s, x1, y1, x2, y2, c);
+}
+
+void ec_allegro_graphic_line(BITMAP *s, int x1, int y1, int x2, int y2, int c)
+{
+    _scale_and_call(line, s, x1, y1, x2, y2, c);
 }
 
 void ec_allegro_graphic_rectfill(BITMAP *s, int x1, int y1, int x2, int y2, int c)
 {
-    x1 = ec_allegro_graphic_scale_x_coord_to_pxl(x1);
-    y1 = ec_allegro_graphic_scale_y_coord_to_pxl(y1);
-    x2 = ec_allegro_graphic_scale_x_coord_to_pxl(x2);
-    y2 = ec_allegro_graphic_scale_y_coord_to_pxl(y2);
-
-    rectfill(s, x1, y1, x2, y2, c);
+    _scale_and_call(rectfill, s, x1, y1, x2, y2, c);
 }
 
 int ec_allegro_graphic_is_in_board(int x, int y)
